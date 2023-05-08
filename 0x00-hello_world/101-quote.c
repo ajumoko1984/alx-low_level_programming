c496c3a3f3990e748be0f625d96b37bf40f90a39
Olawale Afeez <bamideleolawale13@gmail.com>
2023-05-08 15:30:22 -0700

    Commit the wrong ones

--- 0x00-hello_world/101-quote.c
+++
@@ -1,9 +0,0 @@
-#include <unistd.h>
-
-int main(void)
-{
-	const char quote[] = "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n";
-	ssize_t len = sizeof(quote) - 1;
-	ssize_t written = write(STDERR_FILRNO, quote,len);
-	return (written == len) ? 1 : -1;
-}
